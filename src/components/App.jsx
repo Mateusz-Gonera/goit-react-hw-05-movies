bb9e928099e9a5286aca500f6d91c07d206f44f1
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-11-15 20:57:32 +0100

    add first Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,10 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        fontSize: 20,
-        color: '#010101',
-        padding: '20px 10px',
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route></Route>
+    </Routes>
   );
 };
