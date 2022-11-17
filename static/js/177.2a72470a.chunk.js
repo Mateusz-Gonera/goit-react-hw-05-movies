f3b7e036accecc5a6dd4c6d597166d22d63f6b28
Mateusz-Gonera <99548853+Mateusz-Gonera@users.noreply.github.com>
2022-11-17 10:46:31 +0000

    Deploying to gh-pages from @ Mateusz-Gonera/goit-react-hw-05-movies@a77d6ac2a0426f66a1c22f719862341d4f27333a 🚀

---
+++ static/js/177.2a72470a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[177],{177:function(e,t,r){r.r(t);var n=r(689),c=r(731),a=r(184);t.default=function(e){var t=e.movies,r=(0,n.TH)();return(0,a.jsx)("ul",{children:t.map((function(e){return(0,a.jsx)("li",{children:(0,a.jsx)(c.rU,{to:"".concat(e.id),state:{from:r},children:e.title})},e.id)}))})}}}]);
+//# sourceMappingURL=177.2a72470a.chunk.js.map
